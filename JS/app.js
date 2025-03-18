d00f47ff0265494cd3d06e27a1fcda7e8f280494
spatriciade <spatriciade@hotmail.com>
2025-03-18 13:37:36 +0100

    Testing DOM

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,28 @@
-console.log("Hola buenos dias");
+//Seleccionar elementos desde la etiqueta
+let nav = document.querySelector("nav");
+//console.log(nav);
+
+//Seleccionar la clase
+let active = document.querySelector(".active");
+//console.log(active);
+
+//Selecciona multiples elementos con la misma etiqueta
+let headersAndParagraphs = document.querySelectorAll("p,h1"); //lista de nodos
+//console.log(headersAndParagraphs[0].innerText); // selecciona a los elementos de la coleccion ( es array) y en primer lugar
+
+//accediendo a elementos hijos mediante children[num hijo]
+
+//nav.children[0];
+//console.log(nav.children[0].children[0].children[0]);
+//console.log(
+//  nav.firstElementChild.firstElementChild.firstElementChild.parentNode
+//    .nextElementSibling.firstElementChild.firstChild.parentNode
+//);
+//console.log(headersAndParagraphs[2]);
+//console.log(headersAndParagraphs[0].nextElementSibling.nextElementSibling.nextElementSibling);
+
+let main = document.querySelector("#first");
+//console.log(main);
+console.log(nav.firstElementChild.lastElementChild.firstElementChild.firstChild);
+
 

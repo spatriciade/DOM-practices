8a59ca34e2983fa0996a2d1c0d7dffbcc1ddcfb0
spatriciade <spatriciade@hotmail.com>
2025-04-07 13:33:02 +0200

    More about Task

--- JS/app.js
+++ JS/app.js
@@ -1,62 +1,21 @@
-//Propagación -events
-
-//document.body.addEventListener("click", () => {
-//  alert("this is the body");
-//});
-//document.querySelector("section").addEventListener("click", () => {
-//  alert("this is the section");
-//});
-//document.querySelector("div").addEventListener("click", () => {
-//  alert("this is the div");
-//});
-//
-//document.body.addEventListener("keydown", (event) => {
-//  console.log(event.key);
-//});
-//
-//document.body.addEventListener("keydown", (event) => {
-//  event.preventDefault();
-//  console.log(event.key);
-//  console.log(event.code);
-//  if (event.key == "Enter") {
-//    alert("Hello World");
-//  }
-//});
-//Sesion 30
-//document.body.addEventListener("click", () => {
-//  document.body.style.backgroundColor = "black";
-//});
-//document.body.addEventListener("click", () => {
-//  alert("Hello World");
-//});
-
-//Ejecutar eventos y medir el tiempo
-//let time1 = 0.0,
-//  time2 = 0.0;
-//document.body.addEventListener("click", (event) => {
-//  console.log(event);
-//  document.body.style.backgroundColor = "black";
-//  time1 = event.timeStamp;
-//  console.log(time1);
-//  console.log(event.currentTarget);
-//});
-//
-//document.body.addEventListener("click", (event) => {
-//  setTimeout(() => {
-//    alert("Hello World");
-//    time2 = event.timeStamp;
-//    console.log(time2);
-//    console.log(time1 - time2);
-//  }, 3000);
-//});
-
-//Cancelling events
-
-//some items like button/form/input have default actions (reload website and clear form, etc)
-//can be cancelled with stop propagation - prevented for - bu default will be cancelled
-
-console.log(document.forms);
-document.forms[0].onsubmit = (event) => {
-  event.preventDefault(); //with this you avoid the action to happen - but the event still there
-  console.log("Hello World!");
-};
+window.addEventListener("load",() =>{
+let id = 0;
+let text= "";
+let alert = document.querySelector(".alert");
+let close = alert.firstElementChild;
+let input = document.querySelector("#task");
+let arrow = document.querySelector(".arrow");
+//console.log(input);
+close.addEventListener("click", () =>{
+    alert.classList.add("dismissible");
+    });
+input.addEventListener("focus",()=>{
+   //console.log("Working!");
+    document.addEventListener("keydown",(event)=>{
+              console.log(event.code);
+        if(event.code == "Enter"  || event.code == "NumpadEnter"){
+            event.preventDefault();
+        }
+    });
+})
+});
